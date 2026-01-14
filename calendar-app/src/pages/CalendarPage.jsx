cddeaf37d0f6d33dcfd40fa0887b8505785f7719
seojuhui <sjuh0828@gmail.com>
2026-01-14 18:31:08 +0900

    이벤트 추가 모달

--- calendar-app/src/pages/CalendarPage.jsx
+++ calendar-app/src/pages/CalendarPage.jsx
@@ -2,6 +2,8 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 export default function Calendar() {
     const onDateClick = (arg) => {
@@ -11,37 +13,65 @@ export default function Calendar() {
         alert()
     }
 
+    //모달 관련 상태
+    const [show, setShow] = useState(false)
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+
     return (
-        <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        headerToolbar={{
-			left: "prev,next today",
-			center: "title",
-			right: "dayGridMonth,timeGridWeek,timeGridDay",
-		}}
-        weekends={true}
-        events={[
-            {
-                id: "uuid-문자열", // 반드시 유니크
-                title: "휴가",
-                start: "2026-01-13T09:00:00",
-                end: "2026-01-14T18:00:00",
-                memo: "줌 링크...",
-                color: "#3a7e29ff" // 선택
-            },
-            {
-                id: "uuid-문자열", // 반드시 유니크
-                title: "회의",
-                start: "2026-01-19T13:00:00",
-                end: "2026-01-19T14:00:00",
-                memo: "줌 링크...",
-                color: "#3b82f6" // 선택
-            },
-        ]}
-        dateClick={onDateClick}
-		eventClick={onEventClick}
-		selectable={true} //드래그 선택 가능
-        />
+        <>
+            <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            initialView="dayGridMonth"
+            headerToolbar={{
+                left: "prev,next today",
+                center: "title",
+                right: "dayGridMonth,timeGridWeek,timeGridDay",
+            }}
+            weekends={true}
+            events={[
+                {
+                    id: "uuid-문자열", // 반드시 유니크
+                    title: "휴가",
+                    start: "2026-01-13T09:00:00",
+                    end: "2026-01-14T18:00:00",
+                    memo: "줌 링크...",
+                    color: "#3a7e29ff" // 선택
+                },
+                {
+                    id: "uuid-문자열", // 반드시 유니크
+                    title: "회의",
+                    start: "2026-01-19T13:00:00",
+                    end: "2026-01-19T14:00:00",
+                    memo: "줌 링크...",
+                    color: "#3b82f6" // 선택
+                },
+            ]}
+            // dateClick={onDateClick}
+            dateClick={handleShow}
+            eventClick={onEventClick}
+            selectable={true} //드래그 선택 가능
+            />
+            {/* ----------------------- 이벤트 등록 Modal ----------------------- */}
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>이벤트 등록</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div>
+                        <div>title</div>
+                        <input />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
